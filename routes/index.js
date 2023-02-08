0adf55e5abae7c8cdb821b673a886b5555e0a5e3
Alex Manet <alexander.manap@gmail.com>
2023-02-08 17:07:40 +0100

    Creada función de enviar mails con nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  scure: true,
+  auth: {
+    user: 'elevenpipeador@gmail.com',
+    pass: 'wyedzuolprkidsrl'
+  },
+})
+
+transporter.verify().then( () => {
+  console.log('Listo para enviar mails')
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/mailsent', function(req, res, next) {
+  transporter.sendMail({
+    from: ('<?>', [req.body.email]),
+    to: 'elevenpipeador@gmail.com',
+    Subject: 'ContactPage',
+    text: req.body.content
+  })
+
+  res.send('Email enviado!');
 });
 
 router.get('/contact', function(req, res, next) {
